340f03ba7a3bc258b6150bae9a564c5f56d6663f
Hamayoun <hamayounemail@gmail.com>
2022-07-21 18:49:53 +0430

    Removed linter errors

--- src/data.test.js
+++ src/data.test.js
@@ -1,14 +1,13 @@
-// jest.mock('./todo');
+const Todos = require('./mokes/todo.js');
 
-const Todos = require ('./mokes/todo.js');
 const mytodo = new Todos();
 
 describe('to add and remove tests', () => {
   test('to check if data is added successfully ', () => {
-    expect(mytodo.input()).toBe("to do added successfully");
+    expect(mytodo.input()).toBe('to do added successfully');
   });
 
   test('to check if data is deleted successfully ', () => {
-    expect(mytodo.deleteTodo()).toBe("to do deleted successfully");
+    expect(mytodo.deleteTodo()).toBe('to do deleted successfully');
   });
 });
--- src/mokes/todo.js
+++ src/mokes/todo.js
@@ -2,22 +2,17 @@ module.exports = class Todos {
     todoArr = [];
 
     input = () => {
-      const description = "to do some work";
+      const description = 'to do some work';
       const completed = false;
       this.todoArr.push({ description, completed });
-      if(this.todoArr.length > 0){
-        return "to do added successfully";
+      if (this.todoArr.length > 0) {
+        return 'to do added successfully';
       }
-      
     };
-
     deleteTodo = () => {
       this.todoArr.splice(0, 1);
-      if(this.todoArr.length === 0) {
-        return "to do deleted successfully";
+      if (this.todoArr.length === 0) {
+        return 'to do deleted successfully';
       }
-      
   };
-
-
 }
